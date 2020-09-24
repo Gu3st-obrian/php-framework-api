3ee8f1200ffe40feebafc147424ad55e938aa8cb
Ricchy Obrien <ricchyobrien@gmail.com>
2020-09-24 23:37:24 +0100

    Chiffrement des données via openssl

---
+++ boot/crypto.php
@@ -0,0 +1,11 @@
+<?php
+return [
+	'active'  => 'yes|no',
+	'algorithm'  => 'once|multi',
+	'complex' => 'yes|no',
+	'app-type' => 'json|text',
+	'start' => 0,
+	'key' => 'PasswordPassword',
+	'iv' => 'dynamic@dynamic@'
+];
+?>
